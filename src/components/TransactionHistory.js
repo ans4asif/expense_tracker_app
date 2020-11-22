1de91d7eaa2e68c62be3740bba0ff68b99dc5aac
ans4asif <71027119+ans4asif@users.noreply.github.com>
2020-11-22 22:31:47 +0500

    Update TransactionHistory.js

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -7,23 +7,23 @@ import { Transactions } from '../components/Transactions';
 export const TransactionHistory = () => {
     const {transactions} = useContext(GlobalContext);
 
-    const getIncome = () => {
-        let income = 0;
-        for (var i = 0; i < transactions.length; i++) {
-            if (transactions[i].amount > 0)
-                income = income + transactions[i].amount
-        }
-        return income;
-    }
+    // const getIncome = () => {
+    //     let income = 0;
+    //     for (var i = 0; i < transactions.length; i++) {
+    //         if (transactions[i].amount > 0)
+    //             income = income + transactions[i].amount
+    //     }
+    //     return income;
+    // }
 
-    const getExpense = () => {
-        let expense = 0;
-        for (var i = 0; i < transactions.length; i++) {
-            if (transactions[i].amount < 0)
-                expense += transactions[i].amount
-        }
-        return expense;
-    }
+    // const getExpense = () => {
+    //     let expense = 0;
+    //     for (var i = 0; i < transactions.length; i++) {
+    //         if (transactions[i].amount < 0)
+    //             expense += transactions[i].amount
+    //     }
+    //     return expense;
+    // }
 
 
     return (
